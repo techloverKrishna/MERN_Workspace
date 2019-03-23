29605bf5b3b3f589a5c84fd7049b94415b7819d1
krishna <krushnabhamare@gmail.com>
2019-03-23 13:54:11 +0530

    examples

---
+++ passport-based-login/config/keys.js
@@ -0,0 +1,8 @@
+// TODO: we can add cluster database stub also
+
+// dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+// const dbPassword = 'mongodb+srv://Krishna:'+ encodeURIComponent('Krish1993') + '@krishbhamare-ifzhf.mongodb.net/test?retryWrites=true';
+// dbPassword = 'mongodb+srv://Krishna:Krish1993@krishbhamare-ifzhf.mongodb.net/test?retryWrites=true';
+module.exports = {
+    mongoURI: 'mongodb://localhost:27017/admin'
+};
